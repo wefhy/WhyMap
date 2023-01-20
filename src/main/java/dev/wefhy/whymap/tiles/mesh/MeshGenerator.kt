97b67e1403b2b048bd8588db15223dac25614181
wefhy <>
2023-01-21 00:40:49 +0100

    generate sides of the faces

--- src/main/java/dev/wefhy/whymap/tiles/mesh/MeshGenerator.kt
+++ src/main/java/dev/wefhy/whymap/tiles/mesh/MeshGenerator.kt
@@ -15,12 +15,12 @@ object MeshGenerator {
         val chunkHeightMap = getChunkHeightmap(chunk.chunkPos)!!
         val faces = chunkHeightMap.mapIndexed { zz, lines ->
             lines.mapIndexed{xx, height ->
-                getTopFace(xx, zz, height)
+                getSideFaces(xx, zz, height) + getTopFace(xx, zz, height)
             }
         }
 
         val mesh = Mesh()
-        mesh.addFaces(faces.flatten())
+        mesh.addFaces(faces.flatten().flatten())
         return mesh.toPython()
 
 
@@ -43,5 +43,30 @@ object MeshGenerator {
             Vertex((x + 1) * faceSize, (z + 1) * faceSize, height * faceSize),
             Vertex((x + 0) * faceSize, (z + 1) * faceSize, height * faceSize),
         )
+    }
+
+    fun getBottomFace(x: Int, z: Int): Face {
+        return Face(
+            Vertex((x + 0) * faceSize, (z + 0) * faceSize, 0f),
+            Vertex((x + 1) * faceSize, (z + 0) * faceSize, 0f),
+            Vertex((x + 1) * faceSize, (z + 1) * faceSize, 0f),
+            Vertex((x + 0) * faceSize, (z + 1) * faceSize, 0f),
+        )
+    }
+
+    fun getSideFaces(x: Int, z: Int, height: Short): List<Face> {
+        val top = getTopFace(x, z, height)
+        val bottom = getBottomFace(x, z)
+        val merged = top.vertices zip bottom.vertices
+
+        return merged.indices.map { i ->
+            val j = (i + 1).mod(merged.size)
+            Face(
+                merged[i].first,
+                merged[i].second,
+                merged[j].second,
+                merged[j].first
+            )
+        }
     }
 }
