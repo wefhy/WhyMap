639005517550f9a5fdc8bfc17e226105b173e094
wefhy <>
2023-01-21 14:12:31 +0100

    generate blender python code that creates correct mesh with UV. just connect textureatlas to get working object

--- src/main/java/dev/wefhy/whymap/tiles/mesh/MeshGenerator.kt
+++ src/main/java/dev/wefhy/whymap/tiles/mesh/MeshGenerator.kt
@@ -8,7 +8,33 @@ import dev.wefhy.whymap.utils.TileZoom
 import dev.wefhy.whymap.utils.chunkPos
 
 object MeshGenerator {
-    const val faceSize: Float = 0.1f
+    private const val faceSize: Float = 0.1f
+
+    private val head = """
+    import bpy, math, mathutils   
+    """.trimIndent()
+
+    private val tail = """
+    m = bpy.data.meshes.new('chunkmesh')
+    o = bpy.data.objects.new('chunkmesh', m)
+    bpy.context.collection.objects.link(o)
+    o.location = bpy.context.scene.cursor.location
+    m.from_pydata(vertices, [], faces)
+    m.update(calc_edges=True)
+    print('mesh loaded')
+    
+    
+    uv_data = o.data.uv_layers.new(name='NewUV').data
+    polys = o.data.polygons
+    #uv_data = o.data.uv_layers.active.data
+    
+    for poly in polys:
+        i = poly.index
+        uv_data[poly.loop_indices[0]].uv = uvs[i][0]
+        uv_data[poly.loop_indices[1]].uv = uvs[i][1]
+        uv_data[poly.loop_indices[2]].uv = uvs[i][2]
+        uv_data[poly.loop_indices[3]].uv = uvs[i][3]
+    """.trimIndent()
 
     context(MapArea)
     fun generateMesh(): String {
@@ -25,11 +51,10 @@ object MeshGenerator {
 
         val mesh = Mesh()
         mesh.addFaces(faces.flatten().flatten())
-        return mesh.toPython()
+        return "$head\n${mesh.toPython()}\n$tail"
     }
 
-//    context(VertexIndexer)
-    fun getTopFace(x: Int, z: Int, height: Short): Face {
+    private fun getTopFace(x: Int, z: Int, height: Short): Face {
         return Face(
             Vertex((x + 0) * faceSize, (z + 0) * faceSize, height * faceSize),
             Vertex((x + 1) * faceSize, (z + 0) * faceSize, height * faceSize),
@@ -38,7 +63,7 @@ object MeshGenerator {
         )
     }
 
-    fun getBottomFace(x: Int, z: Int): Face {
+    private fun getBottomFace(x: Int, z: Int): Face {
         return Face(
             Vertex((x + 0) * faceSize, (z + 0) * faceSize, 0f),
             Vertex((x + 1) * faceSize, (z + 0) * faceSize, 0f),
@@ -47,7 +72,7 @@ object MeshGenerator {
         )
     }
 
-    fun getSideFaces(x: Int, z: Int, height: Short): List<Face> {
+    private fun getSideFaces(x: Int, z: Int, height: Short): List<Face> {
         val top = getTopFace(x, z, height)
         val bottom = getBottomFace(x, z)
         val merged = top.vertices zip bottom.vertices
