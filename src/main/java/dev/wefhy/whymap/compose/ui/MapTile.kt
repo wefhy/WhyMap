4729c1d6b1876557239420dbb35602821e5556ff
wefhy <>
2024-06-10 01:35:31 +0200

    Swap tiles around

--- src/main/java/dev/wefhy/whymap/compose/ui/MapTile.kt
+++ src/main/java/dev/wefhy/whymap/compose/ui/MapTile.kt
@@ -21,30 +21,37 @@ import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import dev.wefhy.whymap.WhyMapMod.Companion.activeWorld
+import dev.wefhy.whymap.utils.LocalTileBlock
 import dev.wefhy.whymap.utils.LocalTileRegion
+import dev.wefhy.whymap.utils.TileZoom
 
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MapTileView(regionTile: LocalTileRegion) {
+    val nRequiredTiles = 3
+
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
     var scale by remember { mutableStateOf(1f) }
     val paint = Paint().apply {
         filterQuality = FilterQuality.None
     }
+    val block = regionTile.getCenter() - LocalTileBlock(offsetX.toInt(), offsetY.toInt(), TileZoom.BlockZoom)
+    val centerTile = block.parent(TileZoom.RegionZoom)
+    val drawOffset =  centerTile.getCenter() - regionTile.getCenter()
 
 //    var tile: MapArea? by remember { mutableStateOf(null) }
     var image: ImageBitmap? by remember { mutableStateOf(null) }
-    println("MapTileView recompose, image: $image, regionTile: $regionTile")
-    LaunchedEffect(regionTile) {
+    println("MapTileView recompose, image: $image, regionTile: $centerTile")
+    LaunchedEffect(centerTile) {
+        image = null
         println("MapTileView LaunchedEffect")
-        activeWorld?.mapRegionManager?.getRegionForTilesRendering(regionTile) {
+        activeWorld?.mapRegionManager?.getRegionForTilesRendering(centerTile) {
             println("MapTileView LaunchedEffect getRegionForTilesRendering, tile: ${this@getRegionForTilesRendering}")
             val tile = this@getRegionForTilesRendering
             image = tile.renderWhyImageNow().imageBitmap
@@ -56,14 +63,14 @@ fun MapTileView(regionTile: LocalTileRegion) {
 //        Text("Tile $tile")
 //        val t: WhyTiledImage? = tile?.renderWhyImageNow()
 //        val image = t?.imageBitmap
-        val dpSize = with(LocalDensity.current) {
-//            DpSize(t?.width?.toDp() ?: 1.dp, t?.height?.toDp() ?: 1.dp)
-            DpSize(image?.width?.toDp() ?: 1.dp, image?.height?.toDp() ?: 1.dp)
-        }
+//        val dpSize = with(LocalDensity.current) {
+////            DpSize(t?.width?.toDp() ?: 1.dp, t?.height?.toDp() ?: 1.dp)
+//            DpSize(image?.width?.toDp() ?: 1.dp, image?.height?.toDp() ?: 1.dp)
+//        }
 
 
 //        WrappedCanvas(modifier = Modifier.size(dpSize)) { size ->
-        Canvas(modifier = Modifier.size(dpSize).clipToBounds().pointerInput(Unit) {
+        Canvas(modifier = Modifier.size(DpSize(400.dp, 400.dp)).clipToBounds().pointerInput(Unit) {
             detectDragGestures { change, dragAmount ->
                 change.consume()
                 offsetX += dragAmount.x / scale
@@ -81,7 +88,7 @@ fun MapTileView(regionTile: LocalTileRegion) {
             image?.let { im ->
                 scale(scale) {
 //                    drawImage(im, topLeft = Offset(offsetX, offsetY))
-                    drawImage(im, dstOffset = IntOffset(offsetX.toInt(), offsetY.toInt()), filterQuality = FilterQuality.None)
+                    drawImage(im, dstOffset = IntOffset(drawOffset.x + offsetX.toInt(), drawOffset.z + offsetY.toInt()), filterQuality = FilterQuality.None)
                 }
             }
 //            drawIntoCanvas { canvas ->
